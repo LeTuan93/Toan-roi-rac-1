00ae22c81a2c0b5293f2a4512dd326cf0b22ada8
Lê Anh Tuấn <122347842+LeTuan93@users.noreply.github.com>
2023-07-25 14:17:44 +0700

    Update TRR Tong Hop Code.cpp

--- TRR Tong Hop Code.cpp
+++ TRR Tong Hop Code.cpp
@@ -1,125 +1,111 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int x[100],t;
-
-void hoanvi(int i){
-     for (int i=1;i<=t;i++) {
-          x[i]=i;
-          cout << i;
-     }
-     cout << " ";
-     while (1){
-          int vt=i;
-          while (x[vt]<x[vt-1]){
-               vt--;
-          }
-          if (vt==1) break;
-          for (int ii=i;ii>=vt;ii--){
-               if (x[ii]>x[vt-1]){
-                    swap(x[ii],x[vt-1]);
-                    break;
-               }
-          }
-          sort(x+vt,x+i+1);
-          for (int i=1;i<=t;i++) cout<<x[i];
-          cout<<" "; 
-     }
-}
-int unused[100];
-void hoanvi2(int i){
-     for (int j=1;j<=t;j++){
-          if (!unused[j]){
-               unused[j]=1;
-               x[i]=j;
-               if (i==t){
-                    for (int i=1;i<=t;i++) cout<<x[i];
-                    cout<<" ";
-               }
-               else hoanvi2(i+1);
-               unused[j]=0;
-          }
-     }
-}
-
+int x[1001],a[1001];
 int n,k;
-void chapckn(int n,int k){
-     for (int i=1;i<=k;i++){
-          x[i]=i;
-          cout<<x[i];
-     }
-     cout<<" ";
-     while (1){
-          int vt=k;
-          while (n-k+vt==x[vt]){
-               vt--;
-          }
-          if (vt==0) break;
-          x[vt]++;
-          for (int i=vt+1;i<=n;i++){
-               x[i]=x[i-1]+1;
-          }
-          for (int i=1;i<=k;i++) cout<<x[i];
-          cout<<" ";
-     }
+int unused[1001];
+
+void backtrackCKN(int cnt){
+	for (int i=x[cnt-1]+1;i<=n-k+cnt;i++){
+		x[cnt]=i;
+		if ( cnt == k){
+			for (int i=1;i<=k;i++){
+				cout << x[a[i]]<<" ";
+			}
+			cout<<endl;
+		}
+		else backtrackCKN(cnt+1);
+	}
+}
+void sinhCKN(){
+	for (int i=1;i<=k;i++){
+		x[i]=a[i];
+	}
+	while (1){
+		for (int i=1;i<=k;i++){
+			cout<<x[i]<<" ";
+		}
+		cout<<endl;
+		int vt = k;
+		while (x[vt]==n-k+vt) vt--;
+		if (vt<1) break;
+		x[vt]++;
+		for (int i=vt+1;i<=k;i++){
+			x[i]=x[i-1]+1;
+		}
+	}
 }
 
-void chapckn2(int i){
-     for (int j=i;j<=n-k+i;j++){
-          if (x[i-1]<j){
-               x[i]=j;
-               if (i==k){
-                    for (int i=1;i<=k;i++) cout<<x[i];
-                    cout<<" ";
-               }
-               else chapckn2(i+1);
-          }    
-     }
+void backtrackNP(int cnt){
+	for (int i=0;i<=1;i++){
+		x[cnt]=i;
+		if ( cnt == n){
+			for (int i=1;i<=n;i++){
+				cout << x[i];
+			}
+			cout<<endl;
+		}
+		else backtrackNP(cnt+1);
+	}
 }
-int cnt=0;
-void nhiphan(int i){
-     for (int i=1;i<=t;i++){
-          x[i]=0;
-          cout<<x[i];
-     }
-     cout<<" ";
-     while (1){
-          int vt=t;
-          while (x[vt]==1){
-               vt--;
-          }
-          if (vt==0) break;
-          x[vt]=1;
-          for (int i=vt+1;i<=t;i++){
-               x[i]=0;
-          }
-          for (int i=1;i<=t;i++) cout<<x[i];
-          cout<<" ";
-     }
+void sinhNP(){
+	for (int i=1;i<=n;i++) x[i]=0;
+	while (1){
+		for (int i=1;i<=n;i++){
+			cout << x[i]<<" ";
+		}
+		cout<<endl;
+		int vt=n;
+		while (x[vt]==1) vt--;
+		if (vt<1) break;
+		x[vt]=1;
+		for (int i=vt+1;i<=n;i++) x[i]=0;
+	}
 }
-void nhiphan2(int i){
-     for (int j=0;j<=1;j++){
-          x[i]=j;
-          if (i==t){
-               for (int i=1;i<=t;i++) cout<<x[i];
-               cout<<" ";
-          }
-          else nhiphan2(i+1);
-     }
+
+void backtrackHoanVi(int cnt){
+	for (int i=1;i<=n;i++){
+		if (unused[i]==0){
+			unused[i]=1;
+			x[cnt] = i;
+			if (cnt == n){
+				for (int i=1;i<=n;i++) cout<<x[i]<<" ";
+				cout<<endl; 
+			}
+			else backtrackHoanVi(cnt+1);
+			unused[i]=0;
+		}
+	}
+}
+void sinhHoanVi(){
+	for (int i=1;i<=n;i++){
+		x[i]=i;
+	}
+	while (1){
+		for (int i=1;i<=n;i++) cout<<x[i]<<" ";
+		cout<<endl;
+		int vt=n-1;
+		while ( vt>0 && x[vt]>x[vt+1]) vt--;
+		if (vt<1) break;
+		int vt2=n;
+		while (x[vt2]<x[vt]) vt2--;
+		swap(x[vt],x[vt2]);
+		sort(x+vt+1,x+n+1);
+	}
 }
 int main(){
-     //cin >> t;
-     //hoanvi(t);
-     //hoanvi2(1);
-     
-     //cin >> n >> k;
-     //chapckn(n,k);
-     //cout<<endl;
-     //chapckn2(1);
-     
-     //cin >> t;
-     //nhiphan(t);
-     //cout<<endl<<cnt;
-     //nhiphan2(1);
-     
+	cin >> n >> k;
+	for (int i=1;i<=k;i++) a[i]=i;
+	cout<<"BACK TRACK CKN :"<<endl;
+	backtrackCKN(1);
+	cout<<"Sinh CKN :"<<endl;
+	sinhCKN();
+	cout<<"BACK TRACK NHI PHAN :"<<endl;
+	backtrackNP(1);
+	cout<<"Sinh NHI PHAN :"<<endl;
+	sinhNP();
+	cout<<"BACK TRACK HOAN VI :"<<endl;
+	backtrackHoanVi(1);
+	cout<<"Sinh HOAN VI :"<<endl;
+	sinhHoanVi();
 }
